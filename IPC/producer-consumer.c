7ab44d37eee606d321be85ead91fb71cb9b3a090
divijg19 <divijganjoo2003@gmail.com>
2026-04-06 22:59:56 +0530

    IPC/producer-consumer.c

---
+++ IPC/producer-consumer.c
@@ -0,0 +1,39 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+
+#define SIZE 5
+
+int buffer[SIZE], in = 0, out = 0;
+sem_t empty, full, mutex;
+
+void *producer(void *p) {
+  int item;
+  for (int i = 0; i < 10; i++) {
+    item = i;
+    sem_wait(&empty);
+    sem_wait(&mutex);
+
+    buffer[in] = item;
+    printf("Produced: %d\n", item);
+    in = (in + 1) % SIZE;
+
+    sem_post(&mutex);
+    sem_post(&full);
+  }
+}
+
+void *consumer(void *c) {
+  int item;
+  for (int i = 0; i < 10; i++) {
+    sem_wait(&full);
+    sem_wait(&mutex);
+
+    item = buffer[out];
+    printf("Consumed: %d\n", item);
+    out = (out + 1) % SIZE;
+
+    sem_post(&mutex);
+    sem_post(&empty);
+  }
+}
